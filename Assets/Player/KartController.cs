ff3ee61c9b5215785d2106b1c1384a46ecdb15c1
gushabo <gustavogvelazquez03@gmail.com>
2026-02-19 00:39:33 -0600

    Player Movement
    Se creo el player
    - Movimiento
    - Velocidad por monedas
    - Drift
    - Reversa

---
+++ Assets/Player/KartController.cs
@@ -0,0 +1,193 @@
+using UnityEngine;
+using Unity.Cinemachine;
+public class KartController : MonoBehaviour
+{
+    [Header("Camera")]
+    public CinemachineCamera vcam;
+    public float baseFov = 75f;
+    public float maxFov = 90f;
+    
+    [Header("Camera Tilt")]
+    public float maxTilt = 8f;
+    public float tiltSpeed = 5f;
+    private float currentTilt;
+    
+    [Header("Movement")]
+    public float acceleration = 15f;
+    public float maxSpeed = 20f;
+    public float turnSpeed = 120f;
+    
+    [Header("Reverse")]
+    public float reverseAcceleration = 8f;
+    public float maxReverseSpeed = 8f;
+
+    [Header("Drift")]
+    public float driftTurnMultiplier = 1.5f;
+    public float driftGrip = 0.5f;
+    private int driftDirection = 0; // -1 izquierda, 1 derecha
+    
+    [Header("Drift Visual")]
+    public Transform visualModel;
+    
+    [Header("Advanced Drift Visual")]
+    public float driftYawAngle = 35f;      // Giro horizontal exagerado
+    public float driftRollAngle = 30f;     // Inclinación lateral (2 ruedas)
+    public float driftVisualSpeed = 8f;    // Qué tan rápido rota
+
+    private float currentYaw;
+    private float currentRoll;
+
+    [Header("Coins Boost")]
+    public int coins = 0;
+    public float speedPerCoin = 0.5f;
+    public int maxCoins = 10;
+
+    private Rigidbody rb;
+
+    private float currentSpeed;
+    private bool isDrifting;
+
+    private float moveInput;
+    private float turnInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Obtencion de inputs pero se va a cambiar seguramente
+        moveInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+
+        // Drifting
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (Mathf.Abs(turnInput) > 0.2f)
+            {
+                isDrifting = true;
+                driftDirection = (int)Mathf.Sign(turnInput);
+            }
+            
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            isDrifting = false;
+            driftDirection = 0;
+        }
+        
+        
+        // ----- Cosas que afectan a la camara -------
+        float speedPercent = currentSpeed / maxSpeed;
+        
+        // Cambio de FOV por velocidad
+        vcam.Lens.FieldOfView = Mathf.Lerp(baseFov, maxFov, speedPercent);
+        
+        // Agregar una inclinacion en la camara cuando se gira
+        float targetTilt = -turnInput * maxTilt;
+        currentTilt = Mathf.Lerp(currentTilt, targetTilt, Time.deltaTime * tiltSpeed);
+
+        // Rotación solo en Z
+        vcam.transform.localRotation = Quaternion.Euler(0f, 0f, currentTilt);
+
+        
+    }
+
+    void FixedUpdate()
+    {
+        HandleMovement();
+        HandleSteering();
+        HandleDriftVisual();
+    }
+
+    void HandleMovement()
+    {
+
+        // Si hay input agrega movimiento
+        if (moveInput > 0)
+        {
+            currentSpeed += acceleration * Time.fixedDeltaTime;
+        }
+        // Si no, desacelera
+        else if(moveInput < 0)
+        {
+            currentSpeed -= reverseAcceleration * Time.fixedDeltaTime;
+        }
+        else
+        {
+            currentSpeed = Mathf.Lerp(currentSpeed, 0, 3f * Time.fixedDeltaTime);
+        }
+        
+        // Toma en cuenta el boost que otorga las monedas y da la velocidad max final
+        float coinBoost = coins * speedPerCoin;
+        float finalMaxSpeed = maxSpeed + coinBoost;
+        
+        // Hace un clamp en la velocidad
+        currentSpeed = Mathf.Clamp(currentSpeed, -maxReverseSpeed, finalMaxSpeed);
+
+        // Agrega velocidad al RigidBody
+        Vector3 forwardMove = transform.forward * currentSpeed;
+        rb.linearVelocity = new Vector3(forwardMove.x, rb.linearVelocity.y, forwardMove.z);
+    }
+
+    void HandleSteering()
+    {
+        if (Mathf.Abs(currentSpeed) <= 0.1f) return;
+
+        float speedPercent = currentSpeed / maxSpeed;
+
+        float turnMultiplier = isDrifting ? driftTurnMultiplier : 1f;
+        
+        float steeringInput = turnInput;
+
+        if (isDrifting)
+        {
+            steeringInput = driftDirection; // BLOQUEA dirección física
+        }
+
+        float direction = Mathf.Sign(currentSpeed);
+        float rotationAmount = steeringInput * turnSpeed * speedPercent * turnMultiplier * direction * Time.fixedDeltaTime;
+
+        Quaternion turnRotation = Quaternion.Euler(0f, rotationAmount, 0f);
+        rb.MoveRotation(rb.rotation * turnRotation);
+
+        if (isDrifting)
+        {
+            Vector3 sidewaysVelocity = Vector3.Project(rb.linearVelocity, transform.right);
+            rb.linearVelocity -= sidewaysVelocity * driftGrip;
+        }
+    }
+
+    void HandleDriftVisual()
+    {
+        float speedPercent = Mathf.Abs(currentSpeed) / maxSpeed;
+
+        if (isDrifting)
+        {
+            float direction = driftDirection;
+
+            float targetYaw = direction * driftYawAngle;
+            float targetRoll = -direction * driftRollAngle * speedPercent;
+
+            currentYaw = Mathf.Lerp(currentYaw, targetYaw, Time.deltaTime * driftVisualSpeed);
+            currentRoll = Mathf.Lerp(currentRoll, targetRoll, Time.deltaTime * driftVisualSpeed);
+        }
+        else
+        {
+            currentYaw = Mathf.Lerp(currentYaw, 0f, Time.deltaTime * driftVisualSpeed);
+            currentRoll = Mathf.Lerp(currentRoll, 0f, Time.deltaTime * driftVisualSpeed);
+        }
+
+        visualModel.localRotation = Quaternion.Euler(0f, currentYaw, currentRoll);
+    }
+
+    
+    // Agrega las monedas
+    public void AddCoin()
+    {
+        coins++;
+        coins = Mathf.Clamp(coins, 0, maxCoins);
+    }
+}
