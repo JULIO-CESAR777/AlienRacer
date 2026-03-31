4b76b75bb443f76a6a3c508c1af84c9e31301855
gushabo <gustavogvelazquez03@gmail.com>
2026-03-31 13:56:29 -0600

    Localizacion
    Localizacion en todo el menu principal

--- Assets/Language/LanguageManager.cs
+++ Assets/Language/LanguageManager.cs
@@ -9,12 +9,24 @@ public class LanguageManager : MonoBehaviour
 
     private void Awake()
     {
-        instance = this;
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(this);
+        LoadLanguage();
     }
 
     [SerializeField] public LANGUAGES currentLanguage;
     public Action<LANGUAGES> OnLanguageChanged;
     
+    private const string LANGUAGE_PREF_KEY = "Language";
     
     public LANGUAGES CurrentLanguage => currentLanguage;
 
@@ -26,6 +38,7 @@ public class LanguageManager : MonoBehaviour
     public void ChangeLanguage(LANGUAGES newLanguage)
     {
         currentLanguage = newLanguage;
+        SaveLanguage();
         OnLanguageChanged?.Invoke(currentLanguage);
     }
 
@@ -55,8 +68,26 @@ public class LanguageManager : MonoBehaviour
         ChangeLanguage(currentLanguage);
     }
     
-}
+    private void SaveLanguage()
+    {
+        PlayerPrefs.SetInt(LANGUAGE_PREF_KEY, (int)currentLanguage);
+        PlayerPrefs.Save();
+    }
+    
+    private void LoadLanguage()
+    {
+        if (!PlayerPrefs.HasKey(LANGUAGE_PREF_KEY))
+            return;
 
+        int savedLanguage = PlayerPrefs.GetInt(LANGUAGE_PREF_KEY);
+
+        if (savedLanguage >= 0 && savedLanguage < (int)LANGUAGES.END)
+        {
+            currentLanguage = (LANGUAGES)savedLanguage;
+        }
+    }
+    
+}
 
 public enum LANGUAGES
 {
