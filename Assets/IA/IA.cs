a7e9355c7af7215e7f0720df83d7886c7e2d514a
Julio Cesar- intelamd <juliomedellin007@hotmail.com>
2026-05-13 14:36:13 -0600

    aaa

--- Assets/IA/IA.cs
+++ Assets/IA/IA.cs
@@ -429,8 +429,8 @@ public class KartObstaculosIA : MonoBehaviour
 
     public void AplicarRalentizacion(float duracion, float multiplicador)
     {
-        
-        print("ralentizacionrayo");
+        //aqui pones las particulas"
+       
         if (tiempoEscudo > 0 || tiempoStun > 0) return;
         tiempoRalentizado = duracion;
         multiplicadorRalentizacion = multiplicador;
