e4cfbd0cd932e4e9e600949561b2d5973420ca57
Julio Cesar- intelamd <juliomedellin007@hotmail.com>
2026-05-23 14:20:35 -0600

    ArreglosTuto

--- Assets/PowerUps/KartPowerUpController.cs
+++ Assets/PowerUps/KartPowerUpController.cs
@@ -717,7 +717,7 @@ private void StopStarVisual()
         }
     }
 
-    private void ConfigurarLineRendererBase(
+    private void ConfigurarLineRendererBase( 
         LineRenderer lr,
         Material material,
         Color colorRayo,
@@ -753,7 +753,7 @@ private void StopStarVisual()
 
         lr.startColor = colorHDRInicio;
         lr.endColor = colorHDRFinal;
-
+r
         lr.numCapVertices = 8;
         lr.numCornerVertices = 8;
     }
