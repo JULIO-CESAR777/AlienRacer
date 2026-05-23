7137b2dc619001574be748fcad02af4f02f8e629
Eva Luna Álvarez <e.lvarez@aol.com>
2026-05-22 18:53:30 -0600

    PausableTrail Script | Need Test | Could not modify Scene

---
+++ Assets/[Particles]/PausableTrail.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PauseableTrail : MonoBehaviour
+{
+    private ParticleSystem ps;
+    private bool wasPaused = false;
+    private MainManager gm;
+
+    private void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        gm = MainManager.GetInstance();
+    }
+
+    private void Update()
+    {
+        if (gm == null) return;
+
+        bool isPaused = gm.gameState != GameState.Play;
+
+        if (isPaused && !wasPaused)
+        {
+            ps.Pause();
+            wasPaused = true;
+        }
+        else if (!isPaused && wasPaused)
+        {
+            ps.Play();
+            wasPaused = false;
+        }
+    }
+}
